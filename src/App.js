93e9bcaf08dfe47ca8ded96ef5aa538836e68ff3
diogonoguiera27 <diogoalvesnogueira3@gmail.com>
2025-07-08 08:53:41 -0300

    carregamento de dados dinamicos

--- src/App.js
+++ src/App.js
@@ -41,6 +41,15 @@ function App() {
       },
       body:JSON.stringify(product)
     })
+
+    // 3  - carregamento dinamico
+
+    const addedProduct = await res.json();
+
+    setProducts((prevProducts) => [...prevProducts, addedProduct ]);
+
+    setName("");
+    setPrice("");
   };
 
   return (
